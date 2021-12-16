6fc2b9efd6d9701c5cb7cd22c311cb57197d9ad3
Raț Adrian <adirat10@gmail.com>
2021-12-16 17:05:52 +0200

    Add source

---
+++ Probleme_Setul_3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probleme_Setul_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            P1();
+        }
+
+        /// <summary>
+        /// Calculati suma elementelor unui vector de n numere citite de la tastatura. Rezultatul se va afisa pe ecran.
+        /// </summary>
+        private static void P1()
+        {
+            int n, suma = 0;
+            int[] v = new int[100];
+            Console.Write("n=");
+            n = int.Parse(Console.ReadLine());
+
+            string line = Console.ReadLine();
+            char[] sep = { ' ', '\n', '\t', '\r' };
+            string[] t = line.Split(sep, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < n; i++)
+            {
+                v[i] = int.Parse(t[i]);
+                suma = suma + v[i];
+            }
+            Console.WriteLine($"Suma elementelor din vector este {suma}");
+        }
+    }
+}
